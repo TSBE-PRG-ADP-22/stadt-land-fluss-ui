139b2dce5405c4609c2e2803e968e19ca53d1b8c
Jan Fässler <jan@faessler.be>
2022-04-01 14:10:05 +0200

    feat(#13): add react bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Button>Test</Button>
+    </div>
   );
 };
 
