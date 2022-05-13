693cb308fcdc90c3302a0e038b55c50cdf902345
Jan Fässler <jan@faessler.be>
2022-05-13 16:27:30 +0200

    feat(#20): add join route for lobby

---
+++ src/pages/LobbyJoin.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import { joinGame } from '../api/lobbyAPI';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const Lobby = () => {
+  const { id } = useParams();  
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [lobby, setLobby] = useLocalStorage('lobby');
+
+  useEffect(() => {
+    if (id) {
+      joinGame(id).then(({status, data}) => {
+        if(status === 200) {
+          setLobby(data);
+          navigate('/lobby');
+        } else {
+          window.alert('Ups, etwas ist schiefgelaufen. War sicher das Backend.')
+        }
+      });
+    }
+  }, [id, navigate, setLobby]);
+
+  return (
+    <Container>
+      <h1>Joining game now...</h1>
+    </Container>
+  );
+};
+
+export default Lobby;
